18f122802082dfeff0f431cc8acafe793c785921
Valerio <corallini.valerio.1999@gmail.com>
2023-02-24 13:15:20 +0100

    Ristorante Java _ Team 3 - Update Beverages

--- ristoranteJava/src/Main.java
+++ ristoranteJava/src/Main.java
@@ -6,14 +6,13 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-
         ArrayList<Beverage> beverages = new ArrayList<>();
 
         Beverage coca_cola = new Beverage("Coca-Cola", "33cl", 2.99);
-        Beverage sprite = new Beverage("Sprite", "33cl", 2.99);
-        Beverage sevenUp = new Beverage("SevenUp", "33cl", 2.99);
-        Beverage sparkling_water = new Beverage("Sparkling Water", "1,5l", 2.99);
-        Beverage strill_water = new Beverage("Coca-Cola", "1,5l", 2.99);
+        Beverage sprite = new Beverage("\nSprite", "33cl", 2.99);
+        Beverage sevenUp = new Beverage("\nSevenUp", "33cl", 2.99);
+        Beverage sparkling_water = new Beverage("\nSparkling Water", "1,5l", 2.99);
+        Beverage strill_water = new Beverage("\nCoca-Cola", "1,5l", 2.99);
 
         beverages.add(coca_cola);
         beverages.add(sprite);
@@ -21,16 +20,16 @@ public class Main {
         beverages.add(sparkling_water);
         beverages.add(strill_water);
 
-        System.out.println(beverages);
+        System.out.println("\nBeverages:" + beverages);
 
         ArrayList<Second_Courses> secondCourses = new ArrayList<>();
         secondCourses.add(new Second_Courses("Scallops with white wine", 15.00));
-        secondCourses.add(new Second_Courses("Fillet with porcine mushrooms", 24.99));
-        secondCourses.add(new Second_Courses("Sliced beef with rucola and grana", 18.99));
-        secondCourses.add(new Second_Courses("Dolphin Thread", 29.99));
-        secondCourses.add(new Second_Courses("Kobe's Thread (not Bryant)", 129.99));
+        secondCourses.add(new Second_Courses("\nFillet with porcine mushrooms", 24.99));
+        secondCourses.add(new Second_Courses("\nSliced beef with rucola and grana", 18.99));
+        secondCourses.add(new Second_Courses("\nDolphin Thread", 29.99));
+        secondCourses.add(new Second_Courses("\nKobe's Thread (not Bryant)", 129.99));
 
-        System.out.println("The second Courses are : \n" + secondCourses);
+        System.out.println("\nThe second Courses are : \n" + secondCourses);
     }
 
 }
