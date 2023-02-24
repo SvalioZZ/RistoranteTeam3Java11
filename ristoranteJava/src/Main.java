143d79d9c9685ba1c8707b411c6e15eb01dab494
Valerio <corallini.valerio.1999@gmail.com>
2023-02-24 13:16:52 +0100

    Ristorante Java _ Team 3 - Update Beverages

--- ristoranteJava/src/Main.java
+++ ristoranteJava/src/Main.java
@@ -6,8 +6,9 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
+        
         ArrayList<Beverage> beverages = new ArrayList<>();
-
+        System.out.println("Meat Based Menu:");
         Beverage coca_cola = new Beverage("Coca-Cola", "33cl", 2.99);
         Beverage sprite = new Beverage("\nSprite", "33cl", 2.99);
         Beverage sevenUp = new Beverage("\nSevenUp", "33cl", 2.99);
@@ -20,7 +21,7 @@ public class Main {
         beverages.add(sparkling_water);
         beverages.add(strill_water);
 
-        System.out.println("\nBeverages:" + beverages);
+        System.out.println("\nBeverages: \n" + beverages);
 
         ArrayList<Second_Courses> secondCourses = new ArrayList<>();
         secondCourses.add(new Second_Courses("Scallops with white wine", 15.00));
