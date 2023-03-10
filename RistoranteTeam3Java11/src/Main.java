8901daeb854e7c630963d180a02108758794f272
Valerio <corallini.valerio.1999@gmail.com>
2023-03-10 20:31:43 +0100

    Update General Menu Instances

--- RistoranteTeam3Java11/src/Main.java
+++ RistoranteTeam3Java11/src/Main.java
@@ -8,27 +8,21 @@ public class Main {
         
         Menu menu = new Menu();
         
-        ArrayList<Beverage> beverages = new ArrayList<>();
-        System.out.println("Meat Based Menu:");
-        Beverage coca_cola = new Beverage("Coca-Cola", "33cl", 2.99);
-        Beverage sprite = new Beverage("Sprite", "33cl", 2.99);
-        Beverage sevenUp = new Beverage("SevenUp", "33cl", 2.99);
-        Beverage sparkling_water = new Beverage("Sparkling Water", "1,5l", 2.99);
-        Beverage still_water = new Beverage("Still Water", "1,5l", 2.99);
-        menu.addToMenuBeverages(coca_cola);
-        menu.addToMenuBeverages(sprite);
-        menu.addToMenuBeverages(sevenUp);
-        menu.addToMenuBeverages(sparkling_water);
-        menu.addToMenuBeverages(still_water);
+        menu.addToMenuBeverages(new Beverage("Coca-Cola", "33cl", 2.99));
+        menu.addToMenuBeverages(new Beverage("Sprite", "33cl", 2.99));
+        menu.addToMenuBeverages(new Beverage("SevenUp", "33cl", 2.99));
+        menu.addToMenuBeverages(new Beverage("Sparkling Water", "1,5l", 2.99));
+        menu.addToMenuBeverages(new Beverage("Still Water", "1,5l", 2.99));
         
         // Valerio out
         
-        ArrayList<FirstCourses> firstCourses = new ArrayList<>();
         menu.addToMenuFirstCourses(new FirstCourses("Pasta Pasta Rigatoni with Carbonara sauce", 14.50));
         menu.addToMenuFirstCourses(new FirstCourses("Pasta Bucatini with Amatriciana sauce", 14.50));
         menu.addToMenuFirstCourses(new FirstCourses("Pasta Pappardella with Ragù Boar sauce", 12.50));
         menu.addToMenuFirstCourses(new FirstCourses("Pasta Spaghetti with Bolognese sauce", 12.50));
         menu.addToMenuFirstCourses(new FirstCourses("Pasta Spaghetti with Shrimp and Porcini Mushrooms", 18.00));
+        
+        // Daniele out
 
         menu.addToMenuSecondCourses(new SecondCourses("Scallops with white wine", 15.00));
         menu.addToMenuSecondCourses(new SecondCourses("Fillet with porcine mushrooms", 24.99));
@@ -38,21 +32,15 @@ public class Main {
 
         // Giovanni ha frnut (he finished)
         
-        ArrayList<Desserts> desserts = new ArrayList<>();
-        Desserts strawberriesCheesecake = new Desserts("Strawberries Cheesecake", 8.00);
-        Desserts chocolateCheesecake = new Desserts("Chocolate Cheesecake", 8.00);
-        Desserts tiramisu = new Desserts("Tiramisù", 10.00);
-        Desserts applePie = new Desserts("Apple Pie", 6.00);
-        Desserts redVelvetCake = new Desserts("Red Velvet Cake", 8.00);
-        Desserts brownie = new Desserts("Brownie", 5.00);
+        menu.addToMenuDesserts(new Desserts("Strawberries Cheesecake", 8.00));
+        menu.addToMenuDesserts(new Desserts("Chocolate Cheesecake", 8.00));
+        menu.addToMenuDesserts(new Desserts("Tiramisù", 10.00));
+        menu.addToMenuDesserts(new Desserts("Apple Pie", 6.00));
+        menu.addToMenuDesserts(new Desserts("Red Velvet Cake", 8.00));
+        menu.addToMenuDesserts(new Desserts("Brownie", 5.00));
 
-        menu.addToMenuDesserts(strawberriesCheesecake);
-        menu.addToMenuDesserts(chocolateCheesecake);
-        menu.addToMenuDesserts(tiramisu);
-        menu.addToMenuDesserts(applePie);
-        menu.addToMenuDesserts(redVelvetCake);
-        menu.addToMenuDesserts(brownie);
-
+        // Stefano out
+        
         ArrayList<Appetizers> appetizers = new ArrayList<>();
         appetizers.add(new Appetizers("Liver crouton", 10.00));
         appetizers.add(new Appetizers("Crouton with tomatoes", 9.99));
