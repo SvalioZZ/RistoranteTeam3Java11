bd9a41cc909a061d5550d8f93f7104a63bada675
michelepc <mangiacotti.fi@gmail.com>
2023-03-22 13:14:18 +0100

    code review 22/03

---
+++ RistoranteTeam3Java11/src/OldProject/pietanze/PortateTypeEnum.java
@@ -0,0 +1,18 @@
+package OldProject.pietanze;
+
+public enum PortateTypeEnum {
+
+    //TODO inseriamo tutti i tipi e poi ci potete mettere una descrizione ecc ecc
+    BEVERAGES("Bevande"),
+    SECOND("Secondi Piatti");
+
+    private final String nome;
+
+    PortateTypeEnum(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
---
+++ RistoranteTeam3Java11/src/OldProject/pietanze/TypeEnum.java
@@ -0,0 +1,6 @@
+package OldProject.pietanze;
+
+public enum TypeEnum {
+
+    //TODO enum del tipo di menu
+}
