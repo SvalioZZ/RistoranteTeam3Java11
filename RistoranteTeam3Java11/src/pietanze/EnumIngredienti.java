05de980adadc2c4c05aa2ad09eba67ca7a0b36fd
Stefano Devoti <stefano.devoti92@gmail.com>
2023-04-05 16:00:40 +0200

    Revert "modificato dessert e EnumIngredienti"
    This reverts commit de4e1bc1a5d77f4d7bd85616233a049fc4be33d0.

--- RistoranteTeam3Java11/src/pietanze/EnumIngredienti.java
+++ RistoranteTeam3Java11/src/pietanze/EnumIngredienti.java
@@ -1,37 +1,11 @@
 package pietanze;
 
 public enum EnumIngredienti {
-    STRAWBERRIES(1, "Fragole"), CHOCALATE(2,"Cioccolato" ), COCOA(3, "Cacao"),
-    COFFEE(4, "Caffè"), BUTTER(5,"Burro"), MILK(6, "Latte"), EGGS(7, "Uova"),
-    SUGAR(8, "Zucchero"), YEAST(9, "Lievito"), MILK_CREAM(10, "Panna"),
-    CREAM_CHEESE(11, "Formaggio Spalmabile"), MASCARPONE_CHEESE(12, "Mascarpone"),
-    FLOUR(13, "Farina"), APPLES(14, "Farina"), DRY_BISCUITS(15, "Biscotti secchi"),
-    SAVOIARDI_BISCUITS(16, "Savoiardi"), HAZELNUTS(17, "Nocciole"), SALT(18, "Sale"),
-    RED_FOOD_COLORING(19, "Colorante alimentare ROSSO"), VANILLA(20, "Vaniglia");
 
-    private int id;
-    private String name;
-
-    EnumIngredienti(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    //TODO sistemare
+    STRAWBERRIES("Strawberries")
+    , CHOCALATE, COCOA, COFFEE, BUTTER, MILK, EGGS, SUGAR, YEAST, MILK_CREAM, CREAM_CHEESE, MASCARPONE_CHEESE,
+    FLOUR, APPLES, DRY_BISCUITS, SAVOIARDI_BISCUITS, HAZELNUTS, SALT, FOOD_COLORING, VANILLA;
 }
 
 
