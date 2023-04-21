103db94a47360a041e8cef531c19cc5ff1afc347
Valerio <corallini.valerio.1999@gmail.com>
2023-04-21 17:22:23 +0200

    Commitata di prova

--- src/pietanze/Portata.java
+++ src/pietanze/Portata.java
@@ -56,39 +56,3 @@ public class Portata {
         System.out.println(ANSI_BLACK_CHARS.getColor() + ANSI_CYAN_BACKGROUND.getColor() + name + " - " + price + ANSI_RESET.getColor());
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
-    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
-    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
-    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
-    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
-    private static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
-    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
-    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m"
-    private static final String ANSI_BLACK_CHARS = "\u001B[30m";
-*/
-
-
