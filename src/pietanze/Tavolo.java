5ca77fc112476a46d4eaf5fc9bf493a193a6c091
Kristian <asdf>
2023-05-03 15:46:50 +0200

    classe tavolo

---
+++ src/pietanze/Tavolo.java
@@ -0,0 +1,52 @@
+package pietanze;
+
+/**
+ * la classe Tavolo
+ * @author Kristian
+ */
+public class Tavolo {
+    private int tavoloId;
+    private int disponibilitaTavolo;
+    private String descrizione;
+
+    /**
+     * costruttore della classe Tavolo
+     * @param tavoloId
+     * @param disponibilitaTavolo
+     * @param descrizione
+     */
+
+    public Tavolo(int tavoloId, int disponibilitaTavolo, String descrizione) {
+        this.tavoloId = tavoloId;
+        this.disponibilitaTavolo = disponibilitaTavolo;
+        this.descrizione = descrizione;
+    }
+
+    /**
+     * getter e setter
+     */
+
+    public double getTavoloId() {
+        return tavoloId;
+    }
+
+    public void setTavoloId(int tavoloId) {
+        this.tavoloId = tavoloId;
+    }
+
+    public int getDisponibilitaTavolo() {
+        return disponibilitaTavolo;
+    }
+
+    public void setDisponibilitaTavolo(int disponibilitaTavolo) {
+        this.disponibilitaTavolo = disponibilitaTavolo;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+}
