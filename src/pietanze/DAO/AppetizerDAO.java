06eec135f5b00a583307ce4f3215e476c0da935b
Kristian <asdf>
2023-05-17 10:24:51 +0200

    created appetizerDAO(non ancora finita)

---
+++ src/pietanze/DAO/AppetizerDAO.java
@@ -0,0 +1,10 @@
+package pietanze.DAO;
+
+/**
+ * classe appetizerDAO in lavorazione
+ */
+public class AppetizerDAO {
+    private static final String db_url = "jdbc:mysql://localhost/";
+    private static final String username = "root";
+    private static final String password = "password";
+}
